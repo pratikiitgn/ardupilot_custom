7695bd792ea08158db45dadb7c72fb996fbfe266
pratikiitgn <pkprajapati5959@gmail.com>
2022-09-23 20:52:54 +0530

    final successful build

--- build/fmuv3/ap_version.h
+++ build/fmuv3/ap_version.h
@@ -6,5 +6,5 @@
 #error ap_version.h should never be included directly. You probably want to include AP_Common/AP_FWVersion.h
 #endif
 
-#define GIT_VERSION "b1a8384e"
-#define CHIBIOS_GIT_VERSION "b1a8384e"
+#define GIT_VERSION "35962cdc"
+#define CHIBIOS_GIT_VERSION "35962cdc"
