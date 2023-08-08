feb61c178edf873c8f08401f712eef5015667a66
pratik <pkprajapati5959@gmail.com>
2023-08-08 06:45:43 +0530

    back_to_home_boy

--- build/CubeOrange/libraries/GCS_MAVLink/include/mavlink/v2.0/common/common.h
+++ build/CubeOrange/libraries/GCS_MAVLink/include/mavlink/v2.0/common/common.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_COMMON.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_COMMON_XML_HASH -315710260460390743
+#define MAVLINK_COMMON_XML_HASH 9072692889483116884
 
 #ifdef __cplusplus
 extern "C" {
