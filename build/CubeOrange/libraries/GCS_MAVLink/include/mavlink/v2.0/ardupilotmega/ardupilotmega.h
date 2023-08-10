222f1f6722a65da04140a58e18c7ab41bb3d2be8
pratik <pkprajapati5959@gmail.com>
2023-08-10 22:59:18 +0530

    first_basic_code

--- build/CubeOrange/libraries/GCS_MAVLink/include/mavlink/v2.0/ardupilotmega/ardupilotmega.h
+++ build/CubeOrange/libraries/GCS_MAVLink/include/mavlink/v2.0/ardupilotmega/ardupilotmega.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_ARDUPILOTMEGA.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_ARDUPILOTMEGA_XML_HASH 1258417068118017445
+#define MAVLINK_ARDUPILOTMEGA_XML_HASH -6684950169390202039
 
 #ifdef __cplusplus
 extern "C" {
